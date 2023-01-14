4662f3c118262ce7b62e7dba0a8c118d21b0725a
Steve Warner <warner@kairosaerospace.com>
2023-01-14 09:33:19 -0800

    using xarray

--- src/InOneWeekend/test.py
+++ src/InOneWeekend/test.py
@@ -1,16 +1,29 @@
 import numpy as np
 from PIL import Image
+import xarray as xr
+import matplotlib.pyplot as plt
+
 
 width = height = 512
 
 # numpy (with for loops)
 lol = np.zeros((width, height, 3), dtype=np.uint8)
 for j in reversed(range(height)):
-    print(f"Scanlines remaining: {j}")
     for i in range(width):
         r = i / width
         g = j / height
         b = .25
         lol[i, j] = (int(r * 255), int(g * 255), int(b * 255))
-newImg1 = Image.fromarray(lol, mode="RGB")
+newImg1 = Image.fromarray(lol)
 newImg1.save("img1.png")
+
+da = xr.DataArray(
+    data=lol,
+    dims=["x", "y", "rgb"],
+    # coords=dict(
+    #     rgb=["r", "g", "b"]
+    # )
+)
+print(da)
+da.plot.imshow()
+plt.show()
